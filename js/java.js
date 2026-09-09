daf72235b34675856822c75d60184333060eb8ca
sebasChika <sebastianchikaa@gmail.com>
2026-09-09 08:57:30 -0500

    subiendo el resto de los archivos

---
+++ js/java.js
@@ -0,0 +1,58 @@
+let gitHub = document.getElementById("gitHub");
+let linkedIn = document.getElementById("linkedIn");
+let youtube = document.getElementById("youTube");
+let ig = document.getElementById("instagram");
+ig.addEventListener("click", irIg);
+youtube.addEventListener("click", youtubeIr);
+linkedIn.addEventListener("click", linkedIr);
+gitHub.addEventListener("click", gitHubIr);
+let negro = document.getElementById("negro");
+
+function gitHubIr() {
+    negro.classList.remove("aparecer");
+    negro.classList.add("desaparecer");
+    setTimeout(() => {
+        window.location.href = "https://github.com/SebasChika";
+    }, 2000)
+
+}
+
+function irIg() {
+    negro.classList.remove("aparecer");
+    negro.classList.add("desaparecer");
+    setTimeout(() => {
+        window.location.href = "https://www.instagram.com/sebaschikaa/";
+    }, 2000)
+
+}
+
+function youtubeIr() {
+    negro.classList.remove("aparecer");
+    negro.classList.add("desaparecer");
+    setTimeout(() => {
+        window.location.href = "https://www.youtube.com/@SebasChika";
+    }, 2000)
+
+}
+
+function linkedIr() {
+    negro.classList.remove("aparecer");
+    negro.classList.add("desaparecer");
+    setTimeout(() => {
+        window.location.href = "https://www.linkedin.com/in/sebastian-alejandro-chica-arias-b40392386";
+    }, 2000)
+
+}
+
+function iniciar() {
+    negro.classList.add("aparecer");
+}
+
+function volverAparecer() {
+    negro.classList.remove("desaparecer");
+    negro.classList.add("aparecer");
+}
+
+
+window.addEventListener("load", iniciar);
+window.addEventListener("pageshow", volverAparecer);
